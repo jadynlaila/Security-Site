2e14c828ba6d350903a317a809ea0cd75a64df7e
Kyle Ehrenfeld <kyleehrenfeld@gmail.com>
2020-12-16 14:31:14 -0700

    oops

--- Login Page/login.js
+++ Login Page/login.js
@@ -5,12 +5,34 @@ function validateForm() {
     var username = document.forms["loginEmail"]["username"].value;
     var password = document.forms["loginEmail"]["password"].value;
 
-    if ((username == "Kill") && (password == "Me")) {
+    if ((username == "indigobird") && (password == "steadfastprotection")) {   //Correct username: indigobird / pass: steadfastprotection
         window.location.href = '../Home Page/home.html';
-        return false;
+    
+    }
+    else if ((username == "Indigobird") && (password == "Steadfastprotection")) {   //allows for first letter to be capitalized
+        window.location.href = '../Home Page/home.html';
+    
+    }
+    else if ((username == "IndigoBird") && (password == "SteadfastProtection")) {   //allows for second word, first letter to be capitalized
+        window.location.href = '../Home Page/home.html';
+    
+    }
+    else if ((username == "IndigoBird") && (password == "SteadfastProtection")) {   //allows for second word, first letter to be capitalized
+        window.location.href = '../Home Page/home.html';
+    
     }
     else {
         alert("Login unsuccessful.");
     }
 
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function openNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
