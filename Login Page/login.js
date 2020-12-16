f695a2331ea5f345c2ffc1e7612fe58477deade8
Kyle Ehrenfeld <kyleehrenfeld@gmail.com>
2020-12-16 15:27:50 -0700

    worked on login page

--- Login Page/login.js
+++ Login Page/login.js
@@ -4,26 +4,14 @@ function validateForm() {
 
     var username = document.forms["loginEmail"]["username"].value;
     var password = document.forms["loginEmail"]["password"].value;
-
     if ((username == "indigobird") && (password == "steadfastprotection")) {   //Correct username: indigobird / pass: steadfastprotection
-        window.location.href = '../Home Page/home.html';
         console.log("state 1");
-    }
-    else if ((username == "Indigobird") && (password == "Steadfastprotection")) {   //allows for first letter to be capitalized
         window.location.href = '../Home Page/home.html';
-        console.log("state 2");
-    }
-    else if ((username == "IndigoBird") && (password == "SteadfastProtection")) {   //allows for second word, first letter to be capitalized
-        window.location.href = '../Home Page/home.html';
-        console.log("state 3");
-    }
-    else if ((username == "IndigoBird") && (password == "SteadfastProtection")) {   //allows for second word, first letter to be capitalized
-        window.location.href = '../Home Page/home.html';
-        console.log("state 4");
+        return false;
     }
     else {
         alert("Login unsuccessful.");
-        console.log("state 5");
+        console.log("state 3");
     }
 
 
